122315d49da1fc7e5d50a047159fb86e7dcd6fdf
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-06-03 14:37:02 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+ARG branch=latest
+FROM cccs/assemblyline-v4-service-base:$branch
+
+ENV SERVICE_PATH urldownloader.urldownloader.URLDownloader
+
+USER root
+
+# Switch to assemblyline user
+USER assemblyline
+
+# Copy service code
+WORKDIR /opt/al_service
+COPY . .
+
+# Patch version in manifest
+ARG version=4.0.0.dev1
+USER root
+RUN sed -i -e "s/\$SERVICE_TAG/$version/g" service_manifest.yml
+
+# Switch to assemblyline user
+USER assemblyline
---
+++ urldownloader/urldownloader.py
@@ -0,0 +1,46 @@
+import requests
+
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.request import ServiceRequest
+from assemblyline_v4_service.common.result import Result
+
+from tempfile import NamedTemporaryFile
+
+
+class URLDownloader(ServiceBase):
+    def __init__(self, config) -> None:
+        super().__init__(config)
+        self.content_type_filter: list = config.get('content_type_filter', [])
+        self.content_type_filter.append(None)
+
+    def fetch_uri(self, uri: str, apply_filter: bool = True) -> str:
+        resp = requests.get(uri)
+        # Only concerned with gathering responses of interest
+        if resp.ok and resp.headers.get('Content-Type') not in self.content_type_filter:
+            resp_fh = NamedTemporaryFile(delete=False)
+            resp_fh.write(resp.content)
+            resp_fh.close()
+            return resp_fh.name
+        return None
+
+    def execute(self, request: ServiceRequest) -> None:
+        result = Result()
+        submitted_url = []
+
+        # Code to be used when responsibility of fetching submitted_url is moved to service from UI
+        # -----------------------------------------------------------------------------------------
+        # submitted_url = request.task.metadata.get('submitted_url')
+        # # Make sure this is the first URL fetched
+        # submitted_url = [(submitted_url, 10000)] if submitted_url and request.task.depth == 0 else []
+
+        # Only concerned with static/dynamic URIs found by prior services
+        tags = request.task.tags
+        urls = tags.get('network.static.uri', []) + tags.get('network.dynamic.uri', []) + submitted_url
+
+        for tag_value, tag_score in sorted(urls, key=lambda x: x[1]):
+            # Write response and attach to submission
+            fp = self.fetch_uri(tag_value, apply_filter=bool(tag_score < 500))
+            request.add_extracted(fp, tag_value, f"Response from {tag_value}",
+                                  safelist_interface=self.api_interface) if fp else None
+
+        request.result = result
