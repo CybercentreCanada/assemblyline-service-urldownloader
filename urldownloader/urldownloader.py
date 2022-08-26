8e04f283f68b0bf35693b0f1a62f891eeb2ddc0f
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-08-26 12:36:34 +0000

    remove content filtering

--- urldownloader/urldownloader.py
+++ urldownloader/urldownloader.py
@@ -16,16 +16,11 @@ SUBMISSION_PROXIES = config.ui.url_submission_proxies
 class URLDownloader(ServiceBase):
     def __init__(self, config) -> None:
         super().__init__(config)
-        self.content_type_filter: list = config.get('content_type_filter', [])
-        self.content_type_filter.append(None)
 
-    def fetch_uri(self, uri: str, apply_filter: bool = True, headers={}) -> Union[str, requests.Response]:
+    def fetch_uri(self, uri: str, headers={}) -> Union[str, requests.Response]:
         resp = requests.head(uri, allow_redirects=True, timeout=10, headers=headers, proxies=SUBMISSION_PROXIES)
         # Only concerned with gathering responses of interest
         if resp.ok:
-            if apply_filter and any(content_type in resp.headers.get('Content-Type')
-                                    for content_type in self.content_type_filter):
-                return
             resp_fh = NamedTemporaryFile(delete=False)
             resp_fh.write(requests.get(uri, allow_redirects=True, headers=headers).content)
             resp_fh.close()
@@ -62,7 +57,7 @@ class URLDownloader(ServiceBase):
             request.temp_submission_data['visited_urls'].append(tag_value)
             # Write response and attach to submission
             try:
-                fp = self.fetch_uri(tag_value, apply_filter=bool(tag_score < 500), headers=headers)
+                fp = self.fetch_uri(tag_value, headers=headers)
                 if isinstance(fp, str):
                     request.add_extracted(fp, tag_value, f"Response from {tag_value}",
                                           safelist_interface=self.api_interface)
@@ -71,7 +66,7 @@ class URLDownloader(ServiceBase):
             except requests.exceptions.ConnectionError as e:
                 exception_table.add_row(TableRow({'URI': tag_value, 'REASON': str(e).split(':')[-1][:-2]}))
 
-            if exception_table.body:
-                result.add_section(exception_table)
+        if exception_table.body:
+            result.add_section(exception_table)
 
         request.result = result
