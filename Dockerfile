801d761ee253616156657c66eb0d0c89b904f6b9
gdesmar <75089569+gdesmar@users.noreply.github.com>
2023-10-13 13:34:25 +0000

    Migrating to Kangooroo

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,14 @@ ENV SERVICE_PATH urldownloader.URLDownloader
 
 USER root
 
-# Install Chrome for headless rendering of HTML documents
-RUN apt update -y && apt install wget && \
+RUN apt update -y && \
+    apt install -y wget default-jre unzip && \
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
-    apt install -y ./google-chrome-stable_current_amd64.deb && rm -f ./google-chrome-stable_current_amd64.deb
+    apt install -y ./google-chrome-stable_current_amd64.deb && \
+    rm -rf /var/lib/apt/lists/* && \
+    wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$(google-chrome --version | awk '{print $NF}')/linux64/chromedriver-linux64.zip && \
+    unzip -j -d /opt/al_service ./chromedriver-linux64.zip chromedriver-linux64/chromedriver && \
+    rm -f ./google-chrome-stable_current_amd64.deb ./chromedriver-linux64.zip
 
 # Switch to assemblyline user
 USER assemblyline
--- urldownloader.py
+++ urldownloader.py
@@ -1,6 +1,7 @@
+import hashlib
 import json
 import os
-import time
+import subprocess
 
 import requests
 import yaml
@@ -13,12 +14,10 @@ from assemblyline_v4_service.common.result import (
     ResultImageSection,
     ResultKeyValueSection,
     ResultOrderedKeyValueSection,
-    ResultSection,
     ResultTableSection,
     TableRow,
 )
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from PIL import UnidentifiedImageError
 
 
 class URLDownloader(ServiceBase):
@@ -53,52 +52,38 @@ class URLDownloader(ServiceBase):
                 ignored_params_section.update_items(data)
                 ignored_params_section.update_items(headers)
 
-            chrome_options = Options()
-            # chrome_options.add_argument("--disable-extensions")
-            chrome_options.add_argument("--headless=new")
-            chrome_options.add_argument("--network_log.preserve-log=true")
-            chrome_options.add_argument("--hide-scrollbars")
-            chrome_options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
-            download_folder = os.path.join(self.working_directory, "downloads")
-            os.makedirs(download_folder, exist_ok=True)
-            chrome_options.add_experimental_option(
-                "prefs",
-                {
-                    "download.default_directory": download_folder,
-                    "download.prompt_for_download": False,
-                    "download.directory_upgrade": True,
-                    "safebrowsing.enabled": True,
-                },
-            )
+            kangooroo_args = ["java", "-jar", "KangoorooStandalone.jar", "-u", request.task.fileinfo.uri_info.uri]
+            subprocess.run(kangooroo_args, capture_output=True)
 
-            driver = webdriver.Chrome(options=chrome_options)
-            driver.set_window_size(1600, 900)
-            driver.get(request.task.fileinfo.uri_info.uri)
+            url_md5 = hashlib.md5(request.task.fileinfo.uri_info.uri.encode()).hexdigest()
 
-            time.sleep(5)
-            time_slept = 5
+            drop_folder = f"result/{url_md5}/drops"
+            drops_content = os.listdir(drop_folder)
+            workspace_name = [folder for folder in drops_content if folder.startswith("workspace_")][0]
+            drops_workspace = os.path.join(drop_folder, workspace_name)
 
-            all_perfs = []
-            redirects = []
-            downloads = {}
-            process_perf(driver.get_log("performance"), all_perfs, downloads, redirects)
-            while any(download_params["state"] != "completed" for download_params in downloads.values()):
-                if time_slept > self.request_timeout:
-                    uncompleted_downloads = ResultSection(
-                        "Download could not complete in the allocated internal time limit", parent=request.result
-                    )
-                    for download_params in downloads.values():
-                        if download_params["state"] != "completed":
-                            uncompleted_downloads.add_line(
-                                f'{download_params["receivedBytes"]}/{download_params["totalBytes"]}'
-                            )
-                    break
-                time.sleep(2)
-                time_slept += 2
-                process_perf(driver.get_log("performance"), all_perfs, downloads, redirects)
+            results_filepath = os.path.join(drops_workspace, "results.json")
+            with open(results_filepath, "r") as f:
+                results = json.load(f)
 
-            screenshot_path = os.path.join(self.working_directory, "screenshot.png")
-            driver.save_screenshot(screenshot_path)
+            # Main result section
+            result_section = ResultOrderedKeyValueSection("Results", parent=request.result)
+            result_section.add_item("response_code", results["response_code"])
+            result_section.add_item("requested_url", results["requested_url"]["url"])
+            result_section.add_item("requested_url_ip", results["requested_url_ip"])
+            result_section.add_item("actual_url", results["actual_url"])
+            result_section.add_item("actual_url_ip", results["actual_url_ip"])
+            add_tag(result_section, "network.static.uri", results["requested_url"]["url"])
+            result_section.add_tag("network.static.ip", results["requested_url_ip"])
+            add_tag(result_section, "network.static.uri", results["actual_url"])
+            result_section.add_tag("network.static.ip", results["actual_url_ip"])
+            if results["requested_url_ip"] != results["actual_url_ip"]:
+                source_file = os.path.join(drops_workspace, "source.html")
+                if os.path.exists(source_file):
+                    request.add_extracted(source_file, "source.html", "Final html page")
+
+            # Screenshot section
+            screenshot_path = os.path.join(drops_workspace, "screenshot.png")
             if os.path.exists(screenshot_path):
                 screenshot_section = ResultImageSection(
                     request, title_text="Screenshot of visited page", parent=request.result
@@ -109,6 +94,105 @@ class URLDownloader(ServiceBase):
                     description=f"Screenshot of {request.task.fileinfo.uri_info.uri}",
                 )
                 screenshot_section.promote_as_screenshot()
+
+            # favicon section
+            favicon_path = os.path.join(drops_workspace, "favicon.ico")
+            if os.path.exists(favicon_path):
+                try:
+                    screenshot_section = ResultImageSection(request, title_text="Favicon of visited page")
+                    screenshot_section.add_image(
+                        path=favicon_path,
+                        name="favicon.ico",
+                        description=f"Favicon of {request.task.fileinfo.uri_info.uri}",
+                    )
+                    request.result.add_section(screenshot_section)
+                except UnidentifiedImageError:
+                    # Kangooroo is sometime giving html page as favicon...
+                    pass
+
+            # Add the har log file
+            har_filepath = os.path.join(drops_workspace, "session.har")
+            request.add_supplementary(har_filepath, "session.har", "Complete session log")
+            with open(har_filepath, "r") as f:
+                entries = json.load(f)["log"]["entries"]
+
+            # Find any downloaded file
+            downloads = {}
+            for name in drops_content:
+                if not name.startswith("download"):
+                    continue
+                with open(os.path.join(drop_folder, name), "rb") as f:
+                    file_md5 = hashlib.md5(f.read()).hexdigest()
+                downloads[file_md5] = {"path": os.path.join(drop_folder, name), "type": "download"}
+
+            redirects = []
+            for entry in entries:
+                # print(entry)
+
+                # Convert Kangooroo's list of header to a proper dictionary
+                entry["request"]["headers"] = {
+                    header["name"]: header["value"] for header in entry["request"]["headers"]
+                }
+                entry["response"]["headers"] = {
+                    header["name"]: header["value"] for header in entry["response"]["headers"]
+                }
+
+                if entry["response"]["status"] in [301, 302, 303, 307, 308]:
+                    redirects.append(
+                        {
+                            "status": entry["response"]["status"],
+                            "redirecting_url": entry["request"]["url"],
+                            "redirecting_ip": entry["serverIPAddress"],
+                            "redirecting_to": entry["response"]["redirectURL"],
+                        }
+                    )
+
+                if "refresh" in entry["response"]["headers"]:
+                    try:
+                        refresh = entry["response"]["headers"]["refresh"].split(";", 1)
+                        if int(refresh[0]) <= 15 and refresh[1].startswith("url="):
+                            redirects.append(
+                                {
+                                    "status": entry["response"]["status"],
+                                    "redirecting_url": entry["request"]["url"],
+                                    "redirecting_ip": entry["serverIPAddress"],
+                                    "redirecting_to": refresh[1][4:],
+                                }
+                            )
+
+                    except Exception:
+                        # Maybe log that we weren't able to parse the refresh
+                        pass
+
+                if "size" in entry["response"]["content"] and entry["response"]["content"]["size"] != 0:
+                    if "comment" in entry["response"]["content"] and entry["response"]["content"]["comment"].startswith(
+                        "removed;md5:"
+                    ):
+                        file_md5 = entry["response"]["content"]["comment"][12:]
+                        if file_md5 in downloads and "Content-Disposition" in entry["response"]["headers"]:
+                            downloads[file_md5]["filename"] = entry["response"]["headers"]["Content-Disposition"]
+                            if downloads[file_md5]["filename"].startswith("attachment; filename="):
+                                downloads[file_md5]["filename"] = downloads[file_md5]["filename"][21:]
+                            downloads[file_md5]["size"] = entry["response"]["content"]["size"]
+                            downloads[file_md5]["url"] = entry["request"]["url"]
+                            downloads[file_md5]["mimeType"] = entry["response"]["content"]["mimeType"]
+                        else:
+                            # What happened with the content?
+                            pass
+                    else:
+                        content = entry["response"]["content"]["text"].encode()
+                        content_md5 = hashlib.md5(content).hexdigest()
+                        content_path = os.path.join(self.working_directory, content_md5)
+                        with open(content_path, "wb") as f:
+                            f.write(content)
+
+                        if content_md5 not in downloads:
+                            downloads[content_md5] = {"path": content_path, "type": "content"}
+
+                        downloads[content_md5]["filename"] = entry["request"]["url"]
+                        downloads[content_md5]["size"] = entry["response"]["content"]["size"]
+                        downloads[content_md5]["url"] = entry["request"]["url"]
+                        downloads[content_md5]["mimeType"] = entry["response"]["content"]["mimeType"]
 
             if redirects:
                 redirect_section = ResultTableSection("Redirections", parent=request.result)
@@ -121,35 +205,32 @@ class URLDownloader(ServiceBase):
                 redirect_section.set_column_order(["status", "redirecting_url", "redirecting_ip", "redirecting_to"])
 
             if downloads:
-                download_section = ResultTableSection("Downloaded file(s)", parent=request.result)
+                download_section = ResultTableSection("Downloaded file(s)")
+                content_section = ResultTableSection("Content file(s)")
                 for download_params in downloads.values():
-                    download_section.add_row(
+                    (download_section if download_params["type"] == "download" else content_section).add_row(
                         TableRow(
                             dict(
-                                suggestedFilename=download_params["suggestedFilename"],
-                                state=download_params["state"],
-                                bytes=f'{download_params["receivedBytes"]}/{download_params["totalBytes"]}',
+                                Filename=download_params["filename"],
+                                Size=download_params["size"],
+                                mimeType=download_params["mimeType"],
                                 url=download_params["url"],
                             )
                         )
                     )
-                    # This could break if it does not use the suggestedFilename, or maybe if there is multiple of
-                    # the same filename, it would add a ' (1)' at the end?
-                    download_path = os.path.join(download_folder, download_params["suggestedFilename"])
-                    file_info = self.identify.fileinfo(download_path, skip_fuzzy_hashes=True)
+                    file_info = self.identify.fileinfo(download_params["path"], skip_fuzzy_hashes=True)
                     if file_info["type"].startswith("archive"):
-                        request.add_extracted(download_path, file_info["sha256"], "Archive from the URI")
+                        request.add_extracted(
+                            download_params["path"], download_params["filename"], download_params["url"]
+                        )
                     else:
-                        request.add_supplementary(download_path, file_info["sha256"], "Downloaded content from the URI")
-
-                download_section.set_column_order(["suggestedFilename", "state", "bytes", "url"])
-            with open(os.path.join(self.working_directory, "all_perfs.json"), "w") as f:
-                f.write(json.dumps(all_perfs))
-            request.add_supplementary(
-                os.path.join(self.working_directory, "all_perfs.json"), "all_perfs.json", "Complete performance log"
-            )
-
-            driver.quit()
+                        request.add_supplementary(
+                            download_params["path"], download_params["filename"], download_params["url"]
+                        )
+                if download_section.body:
+                    request.result.add_section(download_section)
+                if content_section.body:
+                    request.result.add_section(content_section)
         else:
             # Non-GET request
             r = requests.request(
