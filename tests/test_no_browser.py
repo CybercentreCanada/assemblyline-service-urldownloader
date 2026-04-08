b1336bebcadea84a32868d140188113b8efaa161
gdesmar <75089569+gdesmar@users.noreply.github.com>
2026-04-08 18:17:27 +0000

    Switch option name from force_requests to no_browser

---
+++ tests/test_no_browser.py
@@ -0,0 +1,63 @@
+import os
+import tempfile
+from unittest.mock import patch
+
+import pytest
+from assemblyline.common.file import make_uri_file
+from assemblyline.common.importing import load_module_by_path
+from assemblyline_service_utilities.testing.helper import TestHelper
+from assemblyline_v4_service.common.request import ServiceRequest
+from assemblyline_v4_service.common.task import Task
+
+import urldownloader.urldownloader
+
+service_class = load_module_by_path(
+    "urldownloader.urldownloader.URLDownloader", os.path.join(os.path.dirname(__file__), "..")
+)
+TH = TestHelper(service_class, None)
+
+
+@pytest.mark.parametrize(
+    "no_browser, force_requests, kangooroo, http_request",
+    [
+        (None, None, True, False),
+        (None, False, True, False),
+        (None, True, False, True),
+        (False, None, True, False),
+        (True, None, False, True),
+        (False, False, True, False),
+        (False, True, False, True),
+        (True, False, False, True),
+        (True, True, False, True),
+    ],
+)
+def test_no_browser(no_browser, force_requests, kangooroo, http_request):
+    with (
+        tempfile.TemporaryDirectory() as temp_dir,
+        patch.object(urldownloader.urldownloader.URLDownloader, "execute_kangooroo") as mock_kangooroo,
+        patch.object(urldownloader.urldownloader.URLDownloader, "send_http_request") as mock_http_request,
+    ):
+        file_path = make_uri_file(temp_dir, "https://cybercentrecanada.github.io/assemblyline4_docs/")
+        task = Task(TH._create_service_task(file_path, None))
+        if force_requests is not None:
+            task.service_config["force_requests"] = force_requests  # Set the old parameter for backward checking.
+        if no_browser is not None:
+            task.service_config["no_browser"] = no_browser  # Set the new parameter for backward checking.
+        service_request = ServiceRequest(task)
+        service_request._file_path = file_path
+
+        cls = urldownloader.urldownloader.URLDownloader()
+        try:
+            cls.execute(request=service_request)
+        except Exception:
+            pass
+
+        if kangooroo:
+            mock_kangooroo.assert_called_once()
+        else:
+            mock_kangooroo.assert_not_called()
+
+        if http_request:
+            mock_http_request.assert_called_once()
+        else:
+            mock_http_request.assert_not_called()
